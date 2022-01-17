e1208414ce0fea36cf92de148d75a8afb4741e4b
sky <89959016+yeohl@users.noreply.github.com>
2022-01-17 13:44:39 +0900

    0117

---
+++ goorm/0117_세로순서사각형.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int num = 1;
+	int count = 1;
+	
+	cin >> n;
+
+	if (n > 20) {
+		cout << "20 이하의 자연수 입력" << endl;
+	}
+
+	for (int i = 0; i < n; i++) {
+		num = count;
+		for (int j = 0; j < n; j++) {
+
+			num += n;
+			cout << num-n << " ";
+			}
+		count++;
+		cout << endl;
+		}
+	}
+}
