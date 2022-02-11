7afa91bede78b085a6cbad1d3122342ce416666a
sky <89959016+yeohl@users.noreply.github.com>
2022-02-11 21:32:56 +0900

    0211

---
+++ BAEKJOON/10872.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int factorial(int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else
+		return n * factorial(n - 1);
+}
+int main() {
+	int n;
+	scanf("%d\n", &n);
+	printf("%d", factorial(n));
+}
